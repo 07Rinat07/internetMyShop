5341ef58432447586584591286da334ee67a06a2
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-14 21:15:47 +0500

    Install laravel and settings. Creating database tables, filling with initial data

---
+++ database/seeds/BrandTableSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+
+use Illuminate\Database\Seeder;
+
+class BrandTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // создать 4 бренда
+        factory(App\Brand::class, 4)->create();
+    }
+}
---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder {
+    public function run() {
+        // создать 12 товаров
+        factory(App\Product::class, 12)->create();
+    }
+}
